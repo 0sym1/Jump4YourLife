c53e32972c9cc23aba94271e1877eab6dbde280a
ɴɢᴜʏᴇɴ ᴛɪᴇɴ ʜᴜɴɢ ✯ <ngtienhungg.work@gmail.com>
2024-09-16 23:43:35 +0700

    Comment: đặt tên biến chưa rõ ràng
    - Anh k rename được vì sẽ mất reference trong SO của em, đặt lại tên biến phải rõ ràng vào

--- Assets/Scripts/ScriptableObjects/Ground.cs
+++ Assets/Scripts/ScriptableObjects/Ground.cs
@@ -8,6 +8,8 @@ public class Ground : ScriptableObject
     [SerializeField] private Sprite sprite1;
     [SerializeField] private Sprite sprite2;
 
+    // đặt là normalSprite với brokenSprite (ví dụ thế)
+    // đặt tên biến là sprite1 và sprite2 rất tối nghĩa, sau đọc lại code sẽ k hiểu
     public Sprite GetSprite1(){ return sprite1; }
     public Sprite GetSprite2(){ return sprite2; }
 }
