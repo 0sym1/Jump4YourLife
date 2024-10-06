80681f8d2279dae4334d8be0f7f7e92520bd721d
0sym1 <TuanNgoc.0358@mail.com>
2024-10-06 12:26:45 +0700

    update: utils code

---
+++ Assets/Scripts/Common/GameConfig.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameConfig
+{
+    //Name Panel
+    public const String HomePanel_Name = "HomePanel";
+    public const String SettingPanel_Name = "SettingPanel";
+    public const String SkinShopPanel_Name = "SkinShopPanel";
+    public const String Recharge_Name = "RechargePanel";
+    public const String GameOverPanel_Name = "GameOverPanel";
+    public const String PausePanel_Name = "PausePanel";
+
+    // scene
+    public const string HomeScene = "HomeScene";
+    public const string GameScene = "GameScene";
+    
+    // resource
+    public const string PanelResourcePath = "PanelPrefabs/";
+    public const string SkinPlayerDataResourcePath = "SkinPlayerData/";
+    public const string SkinBackgroundDataResourcePath = "SkinBackgroundData/";
+
+    //PlayerPrefs
+    public const string SkinPlayerCurrent = "SkinPlayerCurrent";
+    public const string SkinBackgroundCurrent = "SkinBackgroundCurrent";
+
+    public enum TypeGround{
+        normal,
+        small,
+        fast,
+        diagonal,
+        invisible,
+    }
+}
